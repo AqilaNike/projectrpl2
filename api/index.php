fd50982622851df25b1455f091c6fd474268fbb2
AqilaNike <aqilanikeindriani_2428240130@mhs.mdp.ac.id>
2026-06-14 22:26:25 +0700

    up

--- api/index.php
+++ api/index.php
@@ -1,3 +1,29 @@
 <?php
-// Forward Vercel requests to normal index.php
-require __DIR__ . '/../public/index.php';
+
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+require __DIR__.'/../vendor/autoload.php';
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// Memindahkan direktori storage ke /tmp karena Vercel read-only
+$app->useStoragePath($_ENV['APP_STORAGE'] ?? '/tmp/storage');
+
+// Membuat folder storage yang diperlukan di /tmp jika belum ada
+$storagePaths = [
+    '/tmp/storage/app',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/framework/views',
+    '/tmp/storage/logs',
+];
+
+foreach ($storagePaths as $path) {
+    if (!is_dir($path)) {
+        mkdir($path, 0777, true);
+    }
+}
+
+$app->handleRequest(Request::capture());
